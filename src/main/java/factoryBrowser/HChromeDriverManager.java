659d71e938974674e5c29863f0af4a487ba2e306
nhudtm <nhudtm@gmail.com>
2026-03-25 15:00:27 +0700

    Fix GitHub Actions Chrome session startup in headless mode

--- src/main/java/factoryBrowser/HChromeDriverManager.java
+++ src/main/java/factoryBrowser/HChromeDriverManager.java
@@ -11,6 +11,9 @@ public class HChromeDriverManager implements BrowserFactory{
         options.addArguments("--disable-notifications"); // disable notification FOR EXAMPLE;
         options.addArguments("--lang=vi"); // set language of browser to vietnamese
         options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
         return new ChromeDriver(options);
